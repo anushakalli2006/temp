b2baaec5681278117599a788f14753000948aab8
Anusha <anusha.kalli.2006@gmail.com>
2026-01-15 10:21:23 +0530

    first commit

---
+++ temp_convert.py
@@ -0,0 +1,15 @@
+import sys
+
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+if __name__ == "__main__":
+    try:
+        c = float(sys.argv[1])
+    except IndexError:
+        c = float(input("Enter temperature in Celsius: "))
+    except ValueError:
+        print("Please enter a valid number.")
+        sys.exit(1)
+
+    print("Fahrenheit:", celsius_to_fahrenheit(c))
